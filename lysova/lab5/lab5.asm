0d5288968c8690f0a7f623fcb618b418e9b03694
AlexandraLysova <sashalysova30@gmail.com>
2020-11-24 22:09:36 +0300

    [READY] Lysova lab5

---
+++ lysova/lab5/lab5.asm
@@ -0,0 +1,76 @@
+EOF EQU '$'
+
+AStack  SEGMENT STACK
+    DB 1024 DUP(?)
+AStack  ENDS
+
+DATA    SEGMENT
+    KEEP_CS DW 0    ;для хранения сегмента вектора прерывания
+    KEEP_IP DW 0    ;для хранения смещения вектора прерывания
+    message DB 'I am interrupt $'
+DATA    ENDS
+
+CODE    SEGMENT
+    ASSUME CS:CODE, DS:DATA, SS:AStack
+
+MY_INT PROC FAR
+    push ax ;сохранение изменяемых регистров
+	push dx 
+
+	mov ah,9h ;заносим функцию вывода строки 
+	mov dx, offset message ;заносим адрес самой строки
+    int 21h ;вызываем прервывание, выводя строку
+
+	pop dx ;восстановление регистров
+	pop ax 
+
+	mov al,20h  ;разрешение обработки прерываний 
+	out 20h,al  ;с более низкими уровнями
+
+	iret ;выход из прерывания
+MY_INT ENDP
+
+MAIN PROC FAR
+
+    mov ax, DATA
+    mov ds, ax
+    GETVECTOR:      ;сохраняем вектор прерывания
+    mov ah, 35h     ;функция получения вектора
+    mov al, 08h     ;номер вектора
+    int 21h
+    mov KEEP_CS, es
+    mov KEEP_IP, bx
+
+    SETVECTOR:      ;устанавливаем новый вектор прерывания
+    push ds
+    mov dx, OFFSET MY_INT
+    mov ax, SEG MY_INT
+    mov ds, ax
+    mov ah, 25h ;функция установки вектора
+    mov al, 08h ;номер вектора
+    int 21h
+    pop ds
+
+
+    int 08h
+
+    RESTOREVECTOR:  ;восстанавливаем старый вектор прерывания
+    cli
+    push ds
+    mov dx, KEEP_IP
+    mov ax, KEEP_CS
+    mov ds, ax
+    mov ah, 25h ;функция установки вектора
+    mov al, 08h ;номер вектора
+    int 21h
+    pop ds
+    sti
+
+    mov ah, 4ch ;завершение программы
+    int 21h
+
+MAIN ENDP
+
+CODE ENDS
+
+END MAIN

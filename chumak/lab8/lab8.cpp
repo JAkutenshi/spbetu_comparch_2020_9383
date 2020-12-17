d4216532eaf1d15387a6c65baae1c0906a762b44
MIhail <55198189+Machumak00@users.noreply.github.com>
2020-12-17 11:51:07 +0300

    [READY] Chumak lab8: update cpp

--- chumak/lab8/lab8.cpp
+++ chumak/lab8/lab8.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 int main() {
@@ -11,6 +11,8 @@ int main() {
 
 double cosh(double x) {
     double result;
+    double exp1;
+    double exp2;
     int degree = -13; // для вычисления по другой формуле
     _asm {
         fld x;              //st(0) = x
@@ -30,67 +32,49 @@ double cosh(double x) {
         fldl2e;             //st(0) = log2e
                             //st(1) = x
         fmul;               //st(0) = x * log2e
-        fld st;             //st(0) = x * log2e 
+        fld st;             //st(0) = x * log2e
                             //st(1) = x * log2e
-        frndint;            //st(0) = [x * log2e] 
+        frndint;            //st(0) = [x * log2e]
                             //st(1) = x * log2e
-        fsub st(1), st;     //st(0) = [x * log2e] 
+        fsub st(1), st;     //st(0) = [x * log2e]
                             //st(1) = x * log2e - [x * log2e]
-        fxch st(1);         //st(0) = x * log2e - [x * log2e] 
+        fxch st(1);         //st(0) = x * log2e - [x * log2e]
                             //st(1) = [x * log2e]
-        f2xm1;              //st(0) = 2 ** (x * log2e - [x * log2e]) - 1 
+        f2xm1;              //st(0) = 2 ** (x * log2e - [x * log2e]) - 1
                             //st(1) = [x * log2e]
-        fld1;               //st(0) = 1 
-                            //st(1) = 2 ^ (x * log2e - [x * log2e]) - 1 
+        fld1;               //st(0) = 1
+                            //st(1) = 2 ^ (x * log2e - [x * log2e]) - 1
                             //st(2) = [x * log2e]
-        fadd;               //st(0) = 2 ^ (x * log2e - [x * log2e]) 
+        fadd;               //st(0) = 2 ^ (x * log2e - [x * log2e])
                             //st(1) = [x * log2e]
         fscale;             //st(0) = e ^ x st(1) = [x * log2e]
-        fstp st(1);         //st(0) = e ^ x
+        fstp exp1;          //st(0) = e ^ x
 
-        fld x;              //st(0) = x
-        fchs;               //st(0) = -x
-        fldl2e;             //st(0) = log2e 
-                            //st(1) = -x
-        fmul;               //st(0) = -x * log2e
-        fld st;             //st(0) = -x * log2e 
-                            //st(1) = -x * log2e
-        frndint;            //st(0) = [-x * log2e] 
-                            //st(1) = -x * log2e
-        fsub st(1), st;     //st(0) = [-x * log2e] 
-                            //st(1) = -x * log2e - [-x * log2e]
-        fxch st(1);         //st(0) = -x * log2e - [-x * log2e] 
-                            //st(1) = [-x * log2e]
-        f2xm1;              //st(0) = 2 ^ (-x * log2e - [-x * log2e]) - 1 
-                            //st(1) = [-x * log2e]
-        fld1;               //st(0) = 1 
-                            //st(1) = 2 ^ (-x * log2e - [-x * log2e]) - 1 
-                            //st(2) = [-x * log2e]
-        fadd;               //st(0) = 2 ^ (-x * log2e - [-x * log2e]) 
-                            //st(1) = [-x * log2e]
-        fscale;             //st(0) = e ^ -x 
-                            //st(1) = [-x * log2e]
-        fstp st(1);         //st(0) = e ^ -x
-
+        fld1;               //st(0) = 1
+        fld exp1;           //st(0) = e ^ x
+                            //st(1) = 1
+        fdiv;               //st(0) = e ^ -x
+        fld exp1            //st(0) = e ^ x
+                            //st(1) = e ^ -x
         fadd;               //st(0) = e ^ x + e ^ (-x)
-        fld1;               //st(0) = 1 
+        fld1;               //st(0) = 1
                             //st(1) = e ^ x + e ^ (-x)
-        fld1;               //st(0) = 1 
-                            //st(1) = 1 
+        fld1;               //st(0) = 1
+                            //st(1) = 1
                             //st(2) = e ^ x + e ^ (-x)
-        fadd;               //st(0) = 2 
+        fadd;               //st(0) = 2
                             //st(1) = e ^ x + e ^ (-x)
         fdiv;               //st(0) =  (e ^ x + e ^ (-x)) / 2
         jmp end_func;
 
     tiny_x:
         fld1;               //st(0) =  1
-        fld x;              //st(0) =  x 
+        fld x;              //st(0) =  x
                             //st(1) =  1
-        fld x;              //st(0) =  x 
-                            //st(1) =  x 
+        fld x;              //st(0) =  x
+                            //st(1) =  x
                             //st(2) =  1
-        fmul;               //st(0) =  x ^ 2 
+        fmul;               //st(0) =  x ^ 2
                             //st(1) =  1
         fld1;               //st(0) = 1
                             //st(1) =  x ^ 2

229be5f3a556da57762ebfa49bb38ead908504c2
BasilNozdrin <bassvass@gmail.com>
2020-12-17 14:43:22 +0300

    Nozdrin_Vasily_lb4

--- nozdrin/lab4/main.cpp
+++ nozdrin/lab4/main.cpp
@@ -4,7 +4,6 @@
 #include <cstring>
 
 #define NMAX 80
-#define N 80
 
 void init();
 std::string readLine();
@@ -48,53 +47,42 @@ void save(std::string result){
 };
 
 std::string task(char* str_in) {
-  char* str_out = new char[N];
+  char* str_out = new char[NMAX];
   asm(
-    "mov r8, %0         \n"//записываем в регистр адрес начала выходной строки
-    "mov rdi, %1        \n"//записываем в регистр адрес начала входной строки
-"getChar:               \n"//=====================================
+    "mov r8, %0         \n"// r8 = %0
+    "mov rdi, %1        \n"// rdi = %1
+"getChar:               \n"//==================================================
     "mov al, [rdi]      \n"// al = getChar(rdi)
     "inc rdi            \n"// rdi++
     "cmp al, 0          \n"// if (al == '\0')
     "je exit            \n"// break
     "cmp al, 0x80       \n"// if(с < 0x80)
-    "jb Latin           \n"// 
-"Cyrillic:              \n"//=====================================
+    "jb Char            \n"// 
+"WChar:                 \n"//==================================================
     "mov ah, al         \n"// ah = al
     "mov al, [rdi]      \n"// al = getChar(rdi) // read second byte of wchar
     "inc rdi            \n"// rdi++
-    "jmp writeWChar     \n"//                   // but it won't be executed because of this jmp
-    "cmp ax, 0xD090     \n"// if(c < 'А')       // this part deCapitalizes Cyrillic too
-    "jl writeWChar      \n"// print
-    "cmp ax, 0xD0AF     \n"// if(c > 'Я')
-    "jg writeWChar      \n"// print
-    "cmp ax, 0xD09F     \n"// if(c > 'П')
-    "jg Cyrillic2       \n"// 'П' has different offset of lower 'п'
-    "add ax, 0x0020     \n"// add offset
     "jmp writeWChar     \n"// print
-"Cyrillic2:             \n"//=====================================
-    "add ax, 0x00E0     \n"// add offset
-    "jmp writeWChar     \n"// print
-"Latin:                 \n"//=====================================
+"Char:                  \n"//==================================================
     "cmp al, 0x5a       \n"// if(c > 'Z')
-    "jg writeChar       \n"// skip
-    "cmp al, 0x41       \n"// if(c < 'A')
-    "jl doNumber        \n"// doNumber
-    "add al, 0x20       \n"// capitalize(c)
-    "jmp writeChar      \n"// print
-"doNumber:              \n"//=====================================
+    "jg writeChar       \n"// print
     "cmp al, 0x30       \n"// if(c < '0')
-    "jl writeChar       \n"// skip
+    "jl writeChar       \n"// print
+    "cmp al, 0x41       \n"// if(c < 'A')
+    "jl Number          \n"// 
+"Lower:                 \n"//==================================================
+    "add al, 0x20       \n"// lower(c)
+    "jmp writeChar      \n"// print
+"Number:                \n"//==================================================
     "cmp al, 0x39       \n"// if(c > '9')
-    "jg writeChar       \n"// skip
-    "neg al             \n"// al = -al        // 
-    "add al, 0x39       \n"// al = '9' - al   // al = int (al)  // inversed
-    "add al, 0x30       \n"// al = '0' + al   // al = (char) al
-"writeChar:             \n"//=====================================
+    "jg writeChar       \n"// print
+    "neg al             \n"// al = -al           // 0x30 ix '0'
+    "add al, 0x69       \n"// 0x69 = 0x39 + 0x30 // 0x39 is '9'
+"writeChar:             \n"//==================================================
     "mov [r8], al       \n"// *r8 = c       // 1 byte
     "inc r8             \n"// r8++
     "jmp getChar        \n"// loop
-"writeWChar:            \n"//=====================================
+"writeWChar:            \n"//==================================================
     "xchg ah, al        \n"// swap(ah, al)
     "mov [r8], ax       \n"// *r8 = c       //  2 bytes
     "inc r8             \n"// r8++

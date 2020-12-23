78c16e99ee3b165f7d1d4753c8ae22dbe603a6d9
BasilNozdrin <bassvass@gmail.com>
2020-12-24 00:26:01 +0300

    Nozdrin_Vasily_lb5

--- nozdrin/lab5/main.asm
+++ nozdrin/lab5/main.asm
@@ -1,97 +1,100 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
+AStack SEGMENT STACK
+    DW 1024 DUP(?)   ; 1 килобайт
+AStack ENDS
 
-#define NMAX 80
 
-void init();
-std::string readLine();
-void save(std::string result);
-std::string task(char* str_in);
+DATA SEGMENT
+    KEEP_CS DW 0 ; для хранения сегмента
+    KEEP_IP DW 0 ; и смещения вектора прерывания
+    MSG     DB "This is my INT msg",10,13,'$'
+DATA ENDS
 
-int main() {
-  init();
-  std::cout << "Enter line\n";
-  auto input = readLine();
-  char* str = new char[80];
-  strcpy(str, input.data());
-  auto result = task(str);
-  save(result);
-  return 0;
-}
 
-void init(){
-  setlocale(LC_ALL, "Russian");
-  std::cout << "\\Задание: преобразовать все заглавные латинские буквы   \\\n"
-               "\\входной строки в строчные, а десятичные цифры в        \\\n"
-               "\\инверсные, остальные символы входной строки передаются \\\n"
-               "\\во выходную строку непосредственно                     \\\n"
-               "\\Author: BasilNozdrin group 9383                        \\\n";
-};
+CODE SEGMENT
+    ASSUME CS:CODE, DS:DATA, SS:AStack
 
-std::string readLine(){
-  std::string line;
-  std::getline(std::cin, line);
-  line = line.substr(0, NMAX);
-  return line;
-};
 
-void save(std::string result){
-  std::ofstream output("output.txt");
-  std::cout << result << "\n";
-  if(output.is_open()) {
-    output << result << "\n";
-    output.close();
-  }
-};
+SUBR_INT PROC FAR
+    jmp ISTART
+    KEEP_SP DW 0
+    KEEP_SS DW 0
+    KEEP_AX DW 0
+    ISTACK  DW 16 DUP(?)
+ISTART:
+    MOV KEEP_SP, SP
+    MOV KEEP_AX, AX
+    MOV AX, SS
+    MOV KEEP_SS, AX
+    MOV AX, KEEP_AX
+    MOV SP, OFFSET ISTART
+    MOV AX, SEG ISTACK
+    MOV SS, AX
 
-std::string task(char* str_in) {
-  char* str_out = new char[NMAX];
-  asm(
-    "mov r8, %0         \n"// r8 = %0
-    "mov rdi, %1        \n"// rdi = %1
-"getChar:               \n"//==================================================
-    "mov al, [rdi]      \n"// al = getChar(rdi)
-    "inc rdi            \n"// rdi++
-    "cmp al, 0          \n"// if (al == '\0')
-    "je exit            \n"// break
-    "cmp al, 0x80       \n"// if(с < 0x80)
-    "jb Char            \n"// 
-"WChar:                 \n"//==================================================
-    "mov ah, al         \n"// ah = al
-    "mov al, [rdi]      \n"// al = getChar(rdi) // read second byte of wchar
-    "inc rdi            \n"// rdi++
-    "jmp writeWChar     \n"// print
-"Char:                  \n"//==================================================
-    "cmp al, 0x5a       \n"// if(c > 'Z')
-    "jg writeChar       \n"// print
-    "cmp al, 0x30       \n"// if(c < '0')
-    "jl writeChar       \n"// print
-    "cmp al, 0x41       \n"// if(c < 'A')
-    "jl Number          \n"// 
-"Lower:                 \n"//==================================================
-    "add al, 0x20       \n"// lower(c)
-    "jmp writeChar      \n"// print
-"Number:                \n"//==================================================
-    "cmp al, 0x39       \n"// if(c > '9')
-    "jg writeChar       \n"// print
-    "neg al             \n"// al = -al           // 0x30 ix '0'
-    "add al, 0x69       \n"// 0x69 = 0x39 + 0x30 // 0x39 is '9'
-"writeChar:             \n"//==================================================
-    "mov [r8], al       \n"// *r8 = c       // 1 byte
-    "inc r8             \n"// r8++
-    "jmp getChar        \n"// loop
-"writeWChar:            \n"//==================================================
-    "xchg ah, al        \n"// swap(ah, al)
-    "mov [r8], ax       \n"// *r8 = c       //  2 bytes
-    "inc r8             \n"// r8++
-    "inc r8             \n"// r8++
-    "jmp getChar        \n"// loop
-"exit:                  \n"
-    "mov [r8], al       \n"// *r8 = c
-    :"=m"(str_out)// %0
-    :"m"(str_in)  // %1
-  );
-  return std::string(str_out);
-}
+    PUSH AX             ; сохранение изменяемых регистров
+    PUSH DX
+        ;<действия по обработке прерывания>
+        MOV DX, OFFSET MSG
+        MOV AH, 09H
+        INT 21H
+    POP DX
+    POP AX
+
+    MOV KEEP_AX, AX
+    MOV SP, KEEP_SP
+    MOV AX, KEEP_SS
+    MOV SS, AX
+    MOV AX, KEEP_AX
+
+    MOV AL, 20H
+    OUT 20H,AL
+    IRET
+SUBR_INT ENDP
+
+
+Main PROC FAR
+    MOV AX, DATA
+    MOV DS, AX
+
+    MOV AH, 35H                 ; ф-ия получения вектора
+    MOV AL, 1CH                 ; номер прерывания
+    INT 21H
+    MOV KEEP_IP, BX             ; запоминание смещения
+    MOV KEEP_CS, ES             ; и сегмента вектора прерывания
+
+    PUSH DS                     ; сохранение DS
+    MOV DX, OFFSET SUBR_INT     ; смещение для процедуры в DX
+    MOV AX, SEG SUBR_INT        ; сегмент процедуры через AX
+    MOV DS, AX                  ; в DS
+    MOV AH, 25H                 ; функция установки вектора
+    MOV AL, 1CH                 ; номер вектора
+    MOV AL, 60H                 ; номер вектора
+    MOV AL, 23H                 ; номер вектора
+    MOV AL, 08H                 ; номер вектора
+    INT 21H                     ; меняем прерывание
+    POP DS                      ; восстановление DS
+
+    ; Далее может выполняться вызов нового обработчика прерывания.
+label1:
+    MOV AH, 01H
+    INT 21H
+    cmp AL, 21
+    jne label1
+
+
+    ; В конце программы восстанавливается старый вектор прерывания
+    CLI
+    PUSH DS
+    MOV DX, KEEP_IP
+    MOV AX, KEEP_CS
+    MOV DS, AX
+    MOV AH, 25H
+    MOV AL, 1CH
+    INT 21H                     ; восстанавливаем старый вектор прерывания
+    POP DS
+    STI
+
+    MOV AH, 4CH
+    INT 21H
+Main ENDP
+CODE ENDS
+END Main
